a23a4f259d3043fb18a035a31cb0907604b5e82f
EnzoIn <enzit93@hotmail.fr>
2024-08-06 16:55:11 +0200

    Fix not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,8 +8,8 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h2 className=" text-9xl font-bold mb-4 text-primary">404</h2>
-      <p className="text-lg mb-6">La page que vous recherchez n'existe pas.</p>
-       <Link href="/"> <Button text="retourner à la page d'accueil"></Button> </Link> 
+      <p className="text-lg mb-6">La page que vous recherchez n&apos;existe pas.</p>
+       <Link href="/"> <Button text="retourner à la page d&apos;accueil"></Button> </Link> 
     </div>
   );
 }
